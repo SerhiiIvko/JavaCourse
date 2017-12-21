291df30b9f4e4fce3b9f5fc85fd2bccc00b53bb1
ivko <sergii.ivko@gmail.com>
2017-12-21 09:59:01 +0200

    git adjusting

--- src/main/java/homeworks/module3/task3/NextHoursFinder.java
+++ src/main/java/homeworks/module3/task3/NextHoursFinder.java
@@ -1,6 +1,5 @@
 package homeworks.module3.task3;
 
-
 import java.util.Scanner;
 
 public class NextHoursFinder {
@@ -11,11 +10,10 @@ public class NextHoursFinder {
         scanner.close();
 
         int circle = 360;
-        int oneHourCorner = circle / 12;
         double oneMinuteCorner = (double) (circle / 12) / 60;
         double minuteCorner = oneMinuteCorner * y;
-        double hours = y / oneHourCorner;
-        double minutes = (hours - ((int) hours) * 60);
+        double hours = (y / oneMinuteCorner) / 60;
+        double minutes = (y / oneMinuteCorner) % 60;
 
         System.out.println("При введённом значении угла, угол минутной стрелки составит " + minuteCorner
                 + " градусов, Часовая стрелка покажет " + (int) hours + " часов, а минутная "
