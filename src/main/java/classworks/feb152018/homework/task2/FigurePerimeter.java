f4385d8ab6a7e215f10667e126cc57391ed78af7
ivko <sergii.ivko@gmail.com>
2018-02-16 19:44:18 +0200

    works 16.02.2018

--- src/main/java/classworks/feb152018/homework/task2/FigurePerimeter.java
+++ src/main/java/classworks/feb152018/homework/task2/FigurePerimeter.java
@@ -1,10 +1,21 @@
 package classworks.feb152018.homework.task2;
 
 /**
- * 2)Найти периметр фигуры по заданным сторонам AB, AD, и CD (см. рисунок)
+ * 2)Найти периметр фигуры по заданным сторонам AB, AC, и CD (см. рисунок)
  * Определить метод для расчета гипотенузы прямоугольного треугольника по его катетам.
  */
 public class FigurePerimeter {
+    public static double getPerimeter(double ab, double ac, double cd) {
+        double bc = getHypotenuseLength(ab, ac);
+        double db = getHypotenuseLength(bc, cd);
+        return (ab + ac + cd + db);
+    }
 
+    public static double getHypotenuseLength(double firstCatheter, double secondCatheter) {
+        return (Math.sqrt(Math.pow(firstCatheter, 2) + Math.pow(secondCatheter, 2)));
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Perimeter of polygon is " + getPerimeter(3, 4, 5));
+    }
 }
