d85b226c2201f0e74bc72b7a54d95d77ce7c3988
Serhii Ivko <sergii.ivko@gmail.com>
2018-09-19 18:52:54 +0300

    17 sep 2018

---
+++ src/main/java/JavaScriptCourse/sep10_2018/ddd.java
@@ -0,0 +1,47 @@
+package JavaScriptCourse.sep10_2018;
+
+public class ddd {
+    public static void main(String[] args) {
+//        for (short i = 32766; i < 32767; i++) {
+//            System.out.println(i);
+//        }
+//        int [] a = new int[5];
+//        a[1]=2;
+//        a[a[1]++]=3;
+//        System.out.println(a[1]);
+//        short i = 32766;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<32767);
+//        boolean a = true;
+//        boolean b = false;
+//        boolean c = true;
+//        if (a==true){
+//            if(b==true){
+//                if (c==true){
+//                    System.out.println("1");
+//                } else System.out.println("2");
+//                } else if (a&&(b=c)){
+//                    System.out.println("3");
+//                }
+//            }else {
+//            System.out.println("4");
+//        }
+//        int i;
+//        int sum = 0;
+//        for (i = 0; i <=10 ; i++) {
+//            sum +=i;
+//        }
+//        System.out.println(sum);
+//        int s = 1;
+//        for (int i = 5; i > 0; i--) {
+//            s += i;
+//            if (i!=0){
+//                break;
+//            }
+//        }
+//        System.out.println("s = " + s);
+
+    }
+}
